975dc01fbce2382eb28d9e57587a8fbac175ebc2
Kicchiy54 <ryuma20020816@gmail.com>
2017-07-19 22:26:52 +0900

    selectMenuの追加と少し記入

--- src/com/WLA/Menu.java
+++ src/com/WLA/Menu.java
@@ -3,6 +3,7 @@ package com.WLA;
 import java.util.HashMap;
 
 import cn.nukkit.Player;
+import cn.nukkit.level.sound.AnvilFallSound;
 import cn.nukkit.level.sound.ClickSound;
 import cn.nukkit.math.Vector3;
 
@@ -31,14 +32,51 @@ public class Menu{
 
 		Vector3 pos = text.Monitor;//モニターの座標
 
-		if(num == 1){//初期画面(Join時のまま)
-			setMenuNumber(player, 2);
-			text.addText(player, "§l>>§6クエストカウンター§r§l<<\n§r§7クエストの受注\n§aクエストに参加", pos);
-			new Main().addSound(player, new ClickSound(pos));
-		}else if(num == 2){
-			setMenuNumber(player, 1);
-			text.addText(player, "§l>>§6クエストカウンター§r§l<<\n§r§aクエストの受注\n§7クエストに参加", pos);
-			new Main().addSound(player, new ClickSound(pos));
+		switch(num){
+
+			case 1:
+				setMenuNumber(player, 2);
+				text.addText(player, "§l>>§6クエストカウンター§r§l<<\n§r§7クエストの受注\n§aクエストに参加", pos);
+				new Main().addSound(player, new ClickSound(pos));
+				break;
+
+			case 2:
+				setMenuNumber(player, 1);
+				text.addText(player, "§l>>§6クエストカウンター§r§l<<\n§r§aクエストの受注\n§7クエストに参加", pos);
+				new Main().addSound(player, new ClickSound(pos));
+				break;
+		}
+
+	}
+
+
+	public void selectMenu(Player player, int num){//メニューを選択する
+
+		Vector3 pos = text.Monitor;//モニターの座標
+
+		switch(num){
+
+			case 1:
+				setMenuNumber(player, 3);
+				text.addText(player, "§l>>§eクエストの受注§r§l<<\n§r受注するクエストの§aID§rを\n" +
+						"§cチャットで§r発言してください\n" + "§a※SELECTでトップメニューへ", pos);
+				new Main().addSound(player, new ClickSound(pos));
+				new Main().setStatus(player, "WaitingQuestID");
+				break;
+
+			case 2:
+				setMenuNumber(player, 4);
+				text.addText(player, "§l>>§eクエストに参加§r§l<<\n§r§aNPC§rをタップして参加してください\n" +
+						"§a※SELECTでトップメニューへ\n§a※NEXTでNPCを更新", pos);
+				new Main().addSound(player, new ClickSound(pos));
+				break;
+
+			case 3:
+			case 4:
+				setMenuNumber(player, 1);
+				text.addText(player, "§l>>§6クエストカウンター§r§l<<\n§r§aクエストの受注\n§7クエストに参加", pos);
+				new Main().addSound(player, new AnvilFallSound(pos));
+				break;
 		}
 
 	}
